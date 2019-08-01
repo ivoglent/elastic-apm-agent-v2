e6e340f6bb9d2aa4a7627adcb7411b9561060d0f
Long Nguyen (Lazada Group) <long.nguyenviet@lazada.com>
2019-08-01 14:16:34 +0700

    fixed untracked files

--- tests/AgentTest.php
+++ tests/AgentTest.php
@@ -6,16 +6,34 @@ namespace Elastic\Apm\PhpAgent\Tests;
 
 use Elastic\Apm\PhpAgent\Agent;
 use Elastic\Apm\PhpAgent\Config;
+use Elastic\Apm\PhpAgent\Model\Context\DbContext;
+use Elastic\Apm\PhpAgent\Model\Context\HttpContext;
+use Elastic\Apm\PhpAgent\Model\Context\SpanContext;
 use PHPUnit\Framework\TestCase;
 
 class AgentTest extends TestCase
 {
 
     public function testTest() {
-        $config = new Config('Test', '10.0', 'http://192.168.100.101:8200', 'apmtoken');
+        $config = new Config('UnitTest', '10.0', 'http://30.108.132.62:8200', 'apmtoken');
         /** @var \Elastic\Apm\PhpAgent\Agent $agent */
         $agent = new Agent($config);
-        $agent->startTransaction('test', 'test');
+        $agent->startTransaction('test3', 'test');
+
+        $trace = $agent->startTrace('Query select', 'sql');
+        $context = new SpanContext([
+            'db' => new DbContext([
+                'type' => 'query',
+                'statement' => 'SELECT * FROM users'
+            ]),
+            'http' => new HttpContext([
+                'url' => 'http://google.com',
+                'method' => 'GET',
+                'status_code' => 200
+            ])
+        ]);
+        $agent->stopTrace($trace->getId(), $context);
+
         $agent->stopTransaction();
     }
 }
